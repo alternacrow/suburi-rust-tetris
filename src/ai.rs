b407c24b07d6f59a0938542d98099e5bc266a200
alternacrow <k.horita21@gmail.com>
2023-03-10 00:38:53 +0900

    fix: ai ver.3

--- src/ai.rs
+++ src/ai.rs
@@ -35,17 +35,23 @@ pub fn eval(game: &Game) -> Game {
             // インプット情報の取得
             let line = erase_line_count(&game.field); // 消せるライン数
             let height_max = field_height_max(&game.field); // フィールドの高さ
+            let height_diff = diff_in_height(&game.field); // 高低差
+            let dead_space = dead_space_count(&game.field); // デッドスペース数
 
             // 正規化
             let mut line = normalization(line as f64, 0.0, 4.0);
             let mut height_max = 1.0 - normalization(height_max as f64, 0.0, 20.0);
+            let mut height_diff = 1.0 - normalization(height_diff as f64, 0.0, 200.0);
+            let mut dead_space = 1.0 - normalization(dead_space as f64, 0.0, 200.0);
 
             // インプット情報に重み付け
             line *= 100.0;
             height_max *= 1.0;
+            height_diff *= 10.0;
+            dead_space *= 100.0;
 
             // インプット情報を評価
-            let score = line + height_max;
+            let score = line + height_max + height_diff + dead_space;
             if elite.1 < score {
                 // 一番良い個体を記録
                 elite.0 = game;
@@ -93,4 +99,46 @@ fn field_height_max(field: &Field) -> usize {
 // 正規化(Min-Max normalization)する
 fn normalization(value: f64, min: f64, max: f64) -> f64 {
     (value - min) / (max - min)
+}
+
+/**
+ * フィールドの高低差の合計を返す
+ */
+fn diff_in_height(field: &Field) -> usize {
+    let mut diff = 0;
+    let mut top = [0; FIELD_WIDTH - 4];
+    // 各列の一番上の高さを求める
+    for x in 2..FIELD_WIDTH - 2 {
+        for y in 1..FIELD_WIDTH - 2 {
+            if field[y][x] != block_kind::NONE {
+                top[x - 2] = FIELD_HEIGHT - y - 1;
+                break;
+            }
+        }
+    }
+    // 右隣との差を合計する
+    for i in 0..FIELD_WIDTH - 4 - 1 {
+        diff += top[i].abs_diff(top[i + 1])
+    }
+    diff
+}
+
+// デッドスペース数を返す
+fn dead_space_count(field: &Field) -> usize {
+    let mut count = 0;
+    for y in (1..FIELD_HEIGHT - 2).rev() {
+        for x in 2..FIELD_WIDTH - 2 {
+            // 各列の一番下の何もない座標
+            if field[y][x] == block_kind::NONE {
+                for y2 in (2..y).rev() {
+                    // 上にブロックがあるならデッドスペース
+                    if field[y2][x] != block_kind::NONE {
+                        count += 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    count
 }
